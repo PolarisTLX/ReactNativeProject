8c085484f579a38a325db1f04c1346f95318d810
Paul Rail <paul.a.rail@gmail.com>
2019-06-04 22:26:06 -0400

    inline styline versus styling constants

--- App.js
+++ App.js
@@ -8,16 +8,103 @@ class One extends React.Component {
   constructor(props) {
     super(props);
     var navigation = this.props.navigation;
+    this.state = {
+      refresh: false,
+      data: [
+        {
+          name: "Harry",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "James",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "Bobby",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "Carl",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "Jos",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "Owen",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "Winters",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "Ralf",
+          image: "http://i.pravatar.cc/400?img=1"
+        }
+      ]
+    }
+  }
+
+  refreshData = () => {
+    this.setState({
+      refresh: true
+    });
+
+    this.setState({
+      refresh: false,
+      data: [
+        {
+          name: "Updated",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "James",
+          image: "http://i.pravatar.cc/400?img=3"
+        }
+      ]
+    })
   }
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ paddingTop: 40, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>One</Text>
+        <FlatList
+          data={this.state.data}
+          style={{width: '100%'}}
+          onRefresh={this.refreshData}
+          refreshing={this.state.refresh}
+          keyExtractor={ (item, index) => index.toString() }
+          renderItem={({item, index}) => (
+            <View style={[
+              styles.rowStyle,
+              index % 2 > 0 ? styles.itemOdd : styles.itemEven
+            ]}>
+            {/* <Image source={{uri: item.image}} style={{width: 50, height: 50, borderRadius: 25 }} /> */}
+            <Image source={{uri: item.image}} style={styles.image} />
+              <Text style={{marginLeft: 25, lineHeight: 50, fontWeight: 'bold', color: 'blue', opacity: 0.7 }}>{item.name}</Text>
+            </View>
+          )}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50/2
+  },
+  rowStyle: {
+    flexDirection: 'row',
+    paddingHorizontal: 25,
+    paddingVertical: 25,
+    borderColor: 'lightgrey',
+    borderBottomWidth: 1
+  },
+  itemOdd: {
+    backgroundColor: '#f1f1f1'
+  },
+  itemEven: {
+    backgroundColor: 'lightblue'
+  }
+})
 
 class Two extends React.Component {
   constructor(props) {
