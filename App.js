c7a83e9470c50ca9a75b93ad69a7450a373f512d
Paul Rail <paul.a.rail@gmail.com>
2019-06-04 20:59:25 -0400

    FlatList component to display array on screen

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: ["Harry", "James", "Bobby", "Carl", "Jos", "Owen", "Winters", "Ralf"]
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app! Hello Paul!</Text>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={ (item, index) => index.toString() }
+          renderItem={({item, seperators}) => (
+            <View>
+              <Text>{item}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
@@ -14,7 +31,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 50,
+    backgroundColor: '#33ABFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
